15288bb453d24815b99e7aa10a64ce261e67ceb1
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-09-28 18:20:16 +0530

    Update LFSR.v

--- Assignment5/Q1/LFSR.v
+++ Assignment5/Q1/LFSR.v
@@ -1,35 +1,32 @@
 `timescale 10ns/1ps
 
-module LFSR(seed, clkslow, rst)
+module LFSR(nextbit, seed, clkslow, rst, sel);
     // input
     input [3:0] seed ;
     input clkslow ;
     input rst ;
-    
-    wire [4:0] w ;
+    input sel ;
+
+    output nextbit ;
 
     // wire for intermediate
-    wire [3:0] wint ;
+    wire [5:1] w ;
+    wire [4:1] wint ;
 
-    always @(posedge clkslow) 
-    begin
+    mux m3(wint[1], w[1], seed[0], sel) ;
+    DFF dff3(w[2], wint[1], clkslow, rst) ;
 
-        assign w[0] = w[3] ^ w[4] ;
-
-        mux m3 (wint[0], w[0], seed[0], sel) ;
-        DFF dff3 (w[1], wint[0], clkslow, rst) ;
-
-        mux m2 (wint[1], w[1], seed[1], sel) ;
-        DFF dff2 (w[2], wint[1], clkslow, rst) ;
+    mux m2(wint[2], w[2], seed[1], sel) ;
+    DFF dff2(w[3], wint[2], clkslow, rst) ;
         
-        mux m1 (wint[2], w[2], seed[2], sel) ;
-        DFF dff1 (w[3], wint[2], clkslow, rst) ;
+    mux m1(wint[3], w[3], seed[2], sel) ;
+    DFF dff1(w[4], wint[3], clkslow, rst) ;
 
-        mux m0 (wint[3], w[3], seed[3], sel) ;
-        DFF dff3 (w[4], wint[3], clkslow, rst) ;
-        
-    end
+    mux m0(wint[4], w[4], seed[3], sel) ;
+    DFF dff0(w[5], wint[4], clkslow, rst) ;
+
+    assign w[1] = w[4] ^ w[5] ;
+
+    assign q = w[1] ;
     
-
 endmodule
-
