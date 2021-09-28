42a493247b9e41a27ea99e9e48e3e4a25df6fc3d
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-09-28 12:02:03 +0530

    final commit

--- Assignment5/Q2/test_FSM.v
+++ Assignment5/Q2/test_FSM.v
@@ -3,7 +3,7 @@
 module testBench;
     // input as reg variables
     reg reset = 0 ;
-    reg clock = 0 ;
+    reg clock = 1 ;
     reg inpBit ;
 
     // output as wires
@@ -17,14 +17,14 @@ module testBench;
         end
 
     // Instantiating twoBitsComplement
-   // twoBitsComplement inst (.outBit(bitOut), .inpBit(bitIn), .clock(clk), .reset(rst)) ;
+    // Explicit Instantiation for clarity
     twoBitsComplement inst (.bitOut(outBit), .bitIn(inpBit), .clk(clock), .rst(reset)) ;
-    // twoBitsComplement inst (outBit, inpBit, clock, reset) ;
+    
     // testing with initial that runs at the start 
     initial 
         begin
         // $monitor("time = %d, input bit = %b, output bit = %b, reset = %b", $time, inpBit, outBit, reset) ;
-        #1 reset = 1  ;
+        reset = 1  ;
         #2 reset = 0  ;
         #2 inpBit = 1 ;
         #2 inpBit = 0 ;
