5ac26ea5aabf8ada2f91fd8075308b6bb063c888
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-09-28 18:58:02 +0530

    Update test_LFSR.v

--- Assignment5/Q1/test_LFSR.v
+++ Assignment5/Q1/test_LFSR.v
@@ -1,4 +1,25 @@
-'timescale 10ns/1ps
+`timescale 10ns/1ps
 
-module testBench;
+module testbench;
+    // input as reg
+    reg rst = 0;
+    reg [3:0] seed = 0 ;
+    reg clk = 0 ;
+    reg sel = 0 ;
+
+    // output as wire
+    wire bitNext ;
+
+    always 
+    #5 clk = ~clk ;
+
+    // instantiation
+    LFSR ll1(bitNext, seed, clk, rst, sel) ;
+
+    // testing
+    initial 
+    begin
+        #10 seed = 4b'1111 ;
+    end
+
 endmodule
