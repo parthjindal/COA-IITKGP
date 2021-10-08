ef3f2d621f54305cb5af11acd2af1109c4d9b6fd
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-10-08 23:48:16 +0530

    final commit q1

--- Assignment6/Q1/tb1.v
+++ Assignment6/Q1/tb1.v
@@ -16,7 +16,27 @@ barrel_shifter unit_test(in_test, shamt_test, dir_test, out_test) ;
 // setting values for input, direction (left/right shift) , and shift amount 
 initial 
 begin
-    // some values     
+    // setting test values
+
+    // right shift
+    in_test = 8'b10011010 ;
+    dir_test = 0 ;
+    shamt_test = 3'b101 ;
+
+    #100
+    $display("input =%d , shift =%d , direction = %b : output = %d ", in_test, shamt_test, dir_test, out_test) ;
+
+    #100
+    // left shift
+    in_test= 8'b10010011 ;
+    dir_test = 1 ;
+    shamt_test = 3'b111 ;
+
+    #100
+    $display("input =%d , shift =%d , direction = %b : output = %d ", in_test, shamt_test, dir_test, out_test) ;
+
+    #100
+    $finish ;
 end
 
 endmodule 
