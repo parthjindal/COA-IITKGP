b4c883e8bb06f30dca4ea14c1d3eaff81ecdf64e
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-10-08 23:30:54 +0530

    Update tb1.v

--- Assignment6/Q1/tb1.v
+++ Assignment6/Q1/tb1.v
@@ -1,5 +1,22 @@
 `timescale 1ns/1ps
 
 module testbench;
-  // testbench remaining
+
+// output as wires
+wire [7:0] out_test ;
+
+// input as regs
+reg [7:0] in_test ;
+reg dir_test ;
+reg [2:0] shamt_test ;
+
+// test instantiation of barrel shifter
+barrel_shifter(in_test, shamt_test, dir_test, out_test) ;
+
+// setting values for input, direction (left/right shift) , and shift amount 
+initial 
+begin
+    // some values     
+end
+
 endmodule 
