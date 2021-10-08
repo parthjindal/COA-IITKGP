37a6270a76e96ba53324c81799eeeb7c81fd480d
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-10-08 23:31:53 +0530

    Update tb1.v

--- Assignment6/Q1/tb1.v
+++ Assignment6/Q1/tb1.v
@@ -11,7 +11,7 @@ reg dir_test ;
 reg [2:0] shamt_test ;
 
 // test instantiation of barrel shifter
-barrel_shifter(in_test, shamt_test, dir_test, out_test) ;
+barrel_shifter unit_test(in_test, shamt_test, dir_test, out_test) ;
 
 // setting values for input, direction (left/right shift) , and shift amount 
 initial 
