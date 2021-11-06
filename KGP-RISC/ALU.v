69302b9691553a76f04f6f6f38f0864698f14c56
Parth Jindal <pmjindal@gmail.com>
2021-11-06 13:31:06 +0530

    Add main utilities

---
+++ KGP-RISC/ALU.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:12:12 11/05/2021 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU(
+    input [31:0] a,
+	 input [31:0] b,
+	 input [2:0] op,
+	 input [4:0] shamt,
+	 input 
+    );
+
+
+endmodule
