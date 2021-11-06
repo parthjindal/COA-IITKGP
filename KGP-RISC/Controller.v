d2b3ad0510a06b9bdb17bcb71fbc4be458e25081
Parth Jindal <pmjindal@gmail.com>
2021-11-06 17:51:04 +0530

    Add ALU controller

---
+++ KGP-RISC/Controller.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:48:37 11/06/2021 
+// Design Name: 
+// Module Name:    Controller 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Controller(
+    input [5:0] opcode,
+	 output memRead,
+	 output memWrite,
+	 output regWrite,
+	 output [1:0] regDst,
+	 output [1:0] mem2Reg,
+	 output branch,
+	 ouput branch
+    );
+
+
+endmodule
